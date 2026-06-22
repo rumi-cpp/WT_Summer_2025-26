6f0c9118076a8ace2d445460de3b89961ef71e4f
rumi-cpp <irfanahm876@gmail.com>
2026-06-22 17:29:21 +0600

    lab1 code

---
+++ lab1.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ElectroMart - Product Catalogue</title>
+    <meta name="author" content="Irfan Ahmed">
+    <meta name="description" content="ElectroMart Product Catalogue">
+</head>
+
+<body>
+
+    <nav>
+        <a href="#">Home</a> |
+        <a href="#">Products</a> |
+        <a href="#">Contact</a>
+    </nav>
+
+    <hr>
+
+    <header>
+        <h2>ElectroMart Product Catalogue</h2>
+    </header>
+
+    <main>
+
+        <fieldset>
+            <legend>Available Products</legend>
+
+            <table border="1">
+
+                <tr>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                    <th>Action</th>
+                </tr>
+
+                <tr>
+                    <td><img src="https://via.placeholder.com/80"></td>
+                    <td><b>Smartphone Pro X</b></td>
+                    <td>6.5-inch display, 128GB storage, 5000mAh battery</td>
+                    <td>$499</td>
+                    <td><a href="#">Buy Now</a></td>
+                </tr>
+
+                <tr>
+                    <td><img src="https://via.placeholder.com/80"></td>
+                    <td><b>Laptop Ultra</b></td>
+                    <td>Intel i7, 16GB RAM, 512GB SSD</td>
+                    <td>$899</td>
+                    <td><a href="#">Buy Now</a></td>
+                </tr>
+
+                <tr>
+                    <td><img src="https://via.placeholder.com/80"></td>
+                    <td><b>Smart Watch</b></td>
+                    <td>Heart rate, GPS, 7-day battery</td>
+                    <td>$199</td>
+                    <td><a href="#">Buy Now</a></td>
+                </tr>
+
+                <tr>
+                    <td><img src="https://via.placeholder.com/80"></td>
+                    <td><b>Bluetooth Speaker</b></td>
+                    <td>Deep bass, waterproof, 12-hour play</td>
+                    <td>$79</td>
+                    <td><a href="#">Buy Now</a></td>
+                </tr>
+
+                <tr>
+                    <td><img src="https://via.placeholder.com/80"></td>
+                    <td><b>Tablet Air</b></td>
+                    <td>10-inch display, 64GB storage</td>
+                    <td>$299</td>
+                    <td><a href="#">Buy Now</a></td>
+                </tr>
+
+            </table>
+
+            <br>
+
+            <b>Total Products: 5</b>
+
+        </fieldset>
+
+    </main>
+
+    <hr>
+
+    <footer>
+        <p>&copy; 2026 ElectroMart</p>
+        <p>Contact: electromart@gmail.com</p>
+    </footer>
+
+</body>
+</html>
